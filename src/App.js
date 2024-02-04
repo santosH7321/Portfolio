9fe2c245ad2c24834b25d43ab1ad3253b708e106
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-04 23:36:14 +0530

    Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return(
+    <>
+      <h1 className='text-4xl text-sky-500 font-bold flex justify-center items-center'>Hello jee</h1>
+    </>
+  );
+}
+
+export default App;
