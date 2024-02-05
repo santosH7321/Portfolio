bf8f3d9eaa23271387eac190e554c9461f218268
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-02-05 11:41:57 +0530

    crossBtn

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,53 +1,78 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { IoMoonOutline } from "react-icons/io5";
 import { CiLight } from "react-icons/ci";
 import { CgMenuGridO } from "react-icons/cg";
+import { RxCross1 } from "react-icons/rx";
 
 const Nav = () => {
-    const [nightMode, setNightMode] = useState(false);
-    const [menuOpen, setMenuOpen] = useState(false);
-    // Night Mode
-   const nightModeHand = () => {
+  const [nightMode, setNightMode] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  // Night Mode
+  const nightModeHand = () => {
     setNightMode((prevMode) => !prevMode);
-   };
+  };
 
-//    ToggleMenu
-   const toggleMenu = () => {
+  //    ToggleMenu
+  const toggleMenu = () => {
     setMenuOpen((prevMenuOpen) => !prevMenuOpen);
-   };
+  };
 
   return (
-    <div className=''>
-      <div className='flex justify-around items-center p-5 max-md:justify-items-end bg-sky-50'>
-        <div className='text-2xl font-bold text-sky-600 '>
-            <h1>Santosh</h1>
+    <div className="">
+      <div className="flex justify-around items-center p-5  bg-sky-50">
+        <div className="text-2xl font-bold text-sky-600 ">
+          <h1>Santosh</h1>
         </div>
-        <div className='flex justify-center items-center'>
-            <ul className=' Dore flex justify-around items-center gap-10 max-md:hidden'>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Skills</a></li>
-                <li><a href='#'>Qualifications</a></li>
-                <li><a href='#'>Projects</a></li>
-                <li><a href='#'>Contact Me</a></li>
-            </ul>
-            <span className='flex justify-center items-center pl-5'>
-                <button onClick={nightModeHand}>
-                    {
-                        nightMode ? <CiLight /> : <IoMoonOutline />
-                    }
-                </button>
-                {/* onClick={toggleMenu} className={menuOpen ? 'active' : '' */}
-                <button onClick={toggleMenu}  className='md:hidden max-md:block text-2xl pl-5'>
-                    <CgMenuGridO />
-                </button>
-            </span>
-                        
+        <div className="flex justify-center items-center">
+          <ul className="navbar flex justify-around items-center gap-10 max-md:hidden font-medium">
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                Qualifications
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-blue-600">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+          <span className="flex justify-center items-center pl-5">
+            <button onClick={nightModeHand}>
+              {nightMode ? <CiLight /> : <IoMoonOutline />}
+            </button>
+            {/* onClick={toggleMenu} className={menuOpen ? 'active' : '' */}
+            <button
+              onClick={toggleMenu}
+              className="md:hidden max-md:block text-2xl pl-5"
+            >
+              {menuOpen ? <RxCross1 /> : <CgMenuGridO />}
+            </button>
+          </span>
         </div>
       </div>
       <hr />
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
